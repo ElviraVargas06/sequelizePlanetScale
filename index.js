46a89b1fdc97640b36d6981ad21219c10040b2eb
Elvis <elvima0906@gmail.com>
2022-06-24 14:47:09 -0500

    Mensaje de bienvenida

--- index.js
+++ index.js
@@ -10,22 +10,7 @@ import blogRouter from "./routes/blog.route.js"
 
 const app = express();
 
-const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2];
 
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            console.log("😲😲😲 =>", origin);
-            if (!origin || whiteList.includes(origin)) {
-                return callback(null, origin);
-            }
-            return callback(
-                "Error de CORS origin: " + origin + " No autorizado!"
-            );
-        },
-        credentials: true,
-    })
-);
 
 
 //Recibe datos JSON
@@ -33,6 +18,10 @@ app.use(express.json())
 
 app.use("/api/v1/auth",authRouter)
 app.use("/api/v1/blogs", blogRouter);
+
+app.get("/", (req, res)=>{
+    res.send("Bienvenido a mi API!")
+})
 
 const PORT = process.env.PORT || 6000
 app.listen(PORT, () => console.log("🌞🌞🌞🌞 http://localhost:" + PORT))
